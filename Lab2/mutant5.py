36c5033b90bc7b7ff86e5a3f297de1b66085a9f3
Maeve <maeve.lorh@gmail.com>
2021-02-11 18:33:10 +0100

    add mutations

---
+++ Lab2/mutant5.py
@@ -0,0 +1,33 @@
+def insertionSort(array):
+    maxSwap = len(array)
+    i = 1
+    while i < maxSwap:
+        j = i
+        while (j > 0) and (array[j] < array[(j - 1)]):
+            tmp = array[j]
+            array[j] = array[j - 1]
+            #instead of array[j - 1] = tmp
+            array[j] = tmp
+            j = j - 1 
+        i = i + 1
+    return array
+
+def memberinArray(element, array):
+    left = 0
+    right = len(array) - 1
+
+    x = (left + right) // 2
+    while array[x] != element and left <= right:
+        if element < array[x]:
+            right = x - 1
+        else:
+            left = x - 1
+        x = (left + right) // 2
+
+    return (array[x] == element)
+
+def III(element, array):
+    sorted = insertionSort(array)
+    memberinArray(element, sorted)
+
+print(III(3, [1, 2, 48, 293293, 909, 3333, 32, 10, 11, 29, 89898, 3, 20]))
