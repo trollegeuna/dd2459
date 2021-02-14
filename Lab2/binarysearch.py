53a9adbdf1556523ea835ccefa83d75a93aa6a30
Trolle Geuna <trolle.geuna@gmail.com>
2021-02-14 17:36:02 +0100

    Update binarysearch.py
    tog bort saker vi skrivit på andra ställen

--- Lab2/binarysearch.py
+++ Lab2/binarysearch.py
@@ -14,33 +14,3 @@ def memberinArray(element, array):
 
 print(memberinArray(6,[1,2,3,4,5]))
 print(memberinArray(4,[1,2,3,4,5]))
-
-
-def memberinArrayMutant1(element, array):
-    left = 0
-    right = len(array) - 1
-
-    x = (left + right) // 2
-    while array[x] != element and left > right:
-        if element < array[x]:
-            right = x - 1
-        else:
-            left = x + 1
-        x = (left + right) // 2
-
-    return (array[x] == element)
-
-
-def memberinArrayMutant2(element, array):
-    left = 0
-    right = len(array) - 1
-
-    x = (left + right) // 2
-    while array[x] != element and left > right:
-        if element < array[x]:
-            right = x - 1
-        else:
-            left = x + 1
-        x = (left + right) // 2
-
-    return (array[x] == element)
